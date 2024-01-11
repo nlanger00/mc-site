0c14dd1f08fee9c74885019f818b33a52d7e9ed7
Nolan Langer <nolan.langer@jackson.com>
2024-01-11 14:45:43 -0500

    log response

--- functions.js
+++ functions.js
@@ -1,11 +1,12 @@
 const heading = document.getElementById("status_display");
 
 function updateStatus(){
-    heading.textContent = "Status Updated"
+    heading.textContent = "Status Updated";
 }
 
 function turnOn(){
-    fetch("https://vjfj5xlovg4riv6k4bpfgnzsmu0tbiuf.lambda-url.us-east-1.on.aws?action=start")
+    let res = fetch("https://vjfj5xlovg4riv6k4bpfgnzsmu0tbiuf.lambda-url.us-east-1.on.aws?action=start");
+    log(res);
 }
 
 function turnOff(){
