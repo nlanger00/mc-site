55b14830b72001359725e8e140ee5510be6b4f12
Nolan Langer <nolan.langer@jackson.com>
2024-01-11 10:27:25 -0500

    js test

---
+++ functions.js
@@ -0,0 +1,13 @@
+const heading = document.getElementById("status_display");
+
+function updateStatus(){
+    heading.textContent = "Status Updated"
+}
+
+function turnOn(){
+
+}
+
+function turnOff(){
+
+}
