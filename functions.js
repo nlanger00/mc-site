78bc4a13407c2f10e778ee91906446577955b0bf
Nolan Langer <nolan.langer@jackson.com>
2024-01-11 16:32:51 -0500

    no-cors specifically for start and stop

--- functions.js
+++ functions.js
@@ -5,7 +5,9 @@ function updateStatus(){
 }
 
 function turnOn(){
-    let req = new Request("https://rzi1g0kdv4.execute-api.us-east-1.amazonaws.com/CORS/start-modded-minecraft?action=start");
+    let req = new Request("https://vjfj5xlovg4riv6k4bpfgnzsmu0tbiuf.lambda-url.us-east-1.on.aws?action=start", {
+        mode: 'no-cors'
+    });
     fetch(req)
     .then((response) => {
         if(response.status === 200){
@@ -17,5 +19,15 @@ function turnOn(){
 }
 
 function turnOff(){
-
+    let req = new Request("https://vjfj5xlovg4riv6k4bpfgnzsmu0tbiuf.lambda-url.us-east-1.on.aws?action=stop", {
+        mode: 'no-cors'
+    });
+    fetch(req)
+    .then((response) => {
+        if(response.status === 200){
+            return response.json();
+        }
+    }).then((response) => {
+        console.debug(response);
+    });
 }
