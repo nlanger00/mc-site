f26ac113e9ce09c14e7c23ed99209314e70cdf7f
Nolan Langer <nolan.langer@jackson.com>
2024-01-11 15:03:19 -0500

    no-cors agan

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@ function updateStatus(){
 function turnOn(){
     let req = new Request("https://vjfj5xlovg4riv6k4bpfgnzsmu0tbiuf.lambda-url.us-east-1.on.aws?action=start", {
         method: "GET",
-        
+        mode: "no-cors"
     });
     fetch(req)
     .then((response) => {
