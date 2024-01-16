e6811c5f529fe8ce040016d7c180808a9416992d
Nolan Langer <nolan.langer@jackson.com>
2024-01-16 10:43:24 -0500

    status

--- functions.js
+++ functions.js
@@ -3,6 +3,14 @@ const heading = document.getElementById("status_display");
 function updateStatus(){
     let req = new Request("https://vjfj5xlovg4riv6k4bpfgnzsmu0tbiuf.lambda-url.us-east-1.on.aws?action=status", {
     });
+    fetch(req)
+    .then((response) => {
+        if(response.status === 200){
+            return response.json();
+        }
+    }).then((response) => {
+        console.debug(response);
+    });
 }
 
 function turnOn(){
