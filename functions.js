2387e8239253553b54a75d332905dd1b49408b78
Nolan Langer <nolan.langer@jackson.com>
2024-01-16 14:52:27 -0500

    starting updates header

--- functions.js
+++ functions.js
@@ -5,22 +5,21 @@ function updateStatus(){
     mode: "cors",
     method: "GET"
     });
-    console.log("here2")
     fetch(req)
     .then((response) => {
         if(response.status === 200){
-            console.log("here1");
             return response.json();
         }
     }).then((response) => {
-        
+        console.debug(response);
         heading.innerHTML = response.InstanceStatuses[0].InstanceState.Name;
     });
 }
 
 function turnOn(){
     let req = new Request("https://vjfj5xlovg4riv6k4bpfgnzsmu0tbiuf.lambda-url.us-east-1.on.aws?action=start", {
-        mode: 'no-cors'
+        mode: "cors",
+        method: "GET"
     });
     fetch(req)
     .then((response) => {
@@ -29,12 +28,14 @@ function turnOn(){
         }
     }).then((response) => {
         console.debug(response);
+        heading.innerHTML = response.StartingInstances[0].CurrentState.Name;
     });
 }
 
 function turnOff(){
     let req = new Request("https://vjfj5xlovg4riv6k4bpfgnzsmu0tbiuf.lambda-url.us-east-1.on.aws?action=stop", {
-        mode: 'no-cors'
+        mode: "cors",
+        method: "GET"
     });
     fetch(req)
     .then((response) => {
