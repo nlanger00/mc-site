e37f177618152da24db107dcd00e62b69dd98ad7
nlanger00 <77987944+nlanger00@users.noreply.github.com>
2024-06-02 18:06:53 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -57,3 +57,15 @@ function turnOff(){
         heading.innerHTML = response.StoppingInstances[0].CurrentState.Name;
     });
 }
+
+function checkPlayerCount(){
+        MinecraftAPI.getServerStatus('44.207.181.221', {
+        port: 25565 // optional, only if you need a custom port
+    }, function (err, status) {
+        if (err) {
+            return document.querySelector('.player-count').innerHTML = 'Error loading status';
+        }
+    
+    // you can change these to your own message!
+    document.querySelector('.player-count').innerHTML = status.players.now;
+}
