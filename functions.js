abb998aa3aff874c1c58d199b45f38929e7b0d77
nlanger00 <77987944+nlanger00@users.noreply.github.com>
2024-06-05 23:21:53 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -77,8 +77,7 @@ function checkPlayerCount(){
 }
 
 function sendCommand(command){
-    command = command.replace("'", "\'");
-    command = command.replace("\"", "\\"");
+    command = escape(command);
     console.log(command);
     let req = new Request("https://vjfj5xlovg4riv6k4bpfgnzsmu0tbiuf.lambda-url.us-east-1.on.aws?action=command&command_data=".concat(command), {
         mode: "cors",
